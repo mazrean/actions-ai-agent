ebd8b4e0068bff96434f8a30c9f7ba7610eb990b
mazrean <shunsuke.w04@gmail.com>
2025-05-11 20:10:14 +0900

    libsql external

--- build.ts
+++ build.ts
@@ -13,4 +13,10 @@ await esbuild.build({
   banner: {
     js: 'import { createRequire } from "module"; import url from "url"; const require = createRequire(import.meta.url); const __filename = url.fileURLToPath(import.meta.url); const __dirname = url.fileURLToPath(new URL(".", import.meta.url));',
   },
+  external: [
+    "@libsql/client",
+    "@libsql/linux-*",
+    "@libsql/darwin-*",
+    "@libsql/win-*",
+  ],
 });
